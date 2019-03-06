e084669c369a2688c4d2dff545de3de025d5b08e
anthony <anthony.dantard@libcast.com>
2019-03-06 14:37:43 +0100

    account support with quota and term

---
+++ src/Model/Quota.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace ApiVideo\Client\Model;
+
+
+class Quota
+{
+    /** @var integer */
+    public $quotaUsed;
+
+    /** @var integer */
+    public $quotaRemaining;
+
+    /** @var integer */
+    public $quotaTotal;
+}
---
+++ src/Model/Term.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace ApiVideo\Client\Model;
+
+use DateTime;
+
+class Term
+{
+    /** @var DateTime */
+    public $startAt;
+
+    /** @var DateTime */
+    public $endAt;
+}
