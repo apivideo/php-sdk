7c23f8fa78f54bfae3600319118e62defce8fc76
antho <anthony.dantard@libcast.com>
2020-05-15 10:23:48 +0200

    reduce chunk size to avoid reaching uploading limit

--- src/Api/Videos.php
+++ src/Api/Videos.php
@@ -25,7 +25,7 @@ class Videos extends BaseApi
     {
         parent::__construct($browser);
 
-        $this->chunkSize = 128 * 1024 * 1024;
+        $this->chunkSize = 120 * 1024 * 1024;
     }
 
     /**
